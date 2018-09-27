ccf69ee43b6da3f77113faaf2792dc30e8e14303
Zachary Lehmann <zlehmann@gmail.com>
2018-09-27 00:07:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,19 +10,19 @@ class Song
     self.class.all << self
   end
 
+  def initialize(name, artist)
+    @name = name
+    @artist = artist
+  end
+
   def self.create
     created_song = Song.new
     created_song.save
     created_song
   end
 
-  def name(name)
-    @name = name
-    @name
-  end
-
   def self.new_by_name(name)
-    created_song = Song.new(@name)
+    created_song = Song.new(name)
     created_song
   end
 end
